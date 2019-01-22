c506735dc97c89994c484958ec5c850b7c648234
colin <colinbowen@outlook.com>
2019-01-22 17:03:28 +0000

    updated SQL functions

--- arkera.py
+++ arkera.py
@@ -1,5 +1,6 @@
 import unittest
 import copy
+import random
 
 
 def increment_dictionary_values(d, i):
@@ -20,10 +21,12 @@ class TestIncrementDictionaryValues (unittest.TestCase):
 
 ####################################################################################################
 
+pricesLst = random.sample(range(1, 10000), 1000)
+
+if
 
 class TestLoss(unittest.TestCase):
     def check_loss(self):
-        pricesLst = [1, 12341, 62456, 25256, 26526, 265265, 25462]
         self.assertLessEqual(pricesLst[2], pricesLst[1])
 
 
@@ -38,37 +41,31 @@ def read(table, **kwargs):
     sql.append(";")
     return "".join(sql)
 
-
-def upsert(table, **kwargs):
-    """ update/insert rows into objects table (update if the row already exists)
-        given the key-value pairs in kwargs """
-    keys = ["%s" % k for k in kwargs]
-    values = ["'%s'" % v for v in kwargs.values()]
+def date(table, date1, date2):
+    """ Generates SQL for a SELECT statement matching the kwargs passed. """
     sql = list()
-    sql.append("INSERT INTO %s (" % table)
-    sql.append(", ".join(keys))
-    sql.append(") VALUES (")
-    sql.append(", ".join(values))
-    sql.append(") ON DUPLICATE KEY UPDATE ")
-    sql.append(", ".join("%s = '%s'" % (k, v) for k, v in kwargs.iteritems()))
+    sql.append("SELECT * FROM %s" % table)
+    if date1 and date2:
+        sql.append("WHERE date BETWEEN " + date1 + " AND " + date2)
+    elif date1:
+        sql.append("WHERE date BETWEEN " + date1 + " < CURRENT_DATE()")
+    elif date2:
+        sql.append("WHERE date BETWEEN " + "01/01/2001 " + date2)
     sql.append(";")
     return "".join(sql)
 
+def id():
+    pass
 
-def delete(table, **kwargs):
-    """ deletes rows from table where **kwargs match """
+def url(urlLink):
+    """ Generates SQL for a SELECT statement matching the kwargs passed. """
     sql = list()
-    sql.append("DELETE FROM %s " % table)
-    sql.append("WHERE " + " AND ".join("%s = '%s'" % (k, v) for k, v in kwargs.iteritems()))
+    sql.append("SELECT * FROM %s " % table)
+    if urlLink:
+        sql.append("WHERE URL = " + urlLink )
     sql.append(";")
     return "".join(sql)
 
-
-# >>> upsert("tbl", LogID=500, LoggedValue=5)
-# "INSERT INTO tbl (LogID, LoggedValue) VALUES ('500', '5') ON DUPLICATE KEY UPDATE LogID = '500', LoggedValue = '5';"
-
-# >>> read("tbl", **{"username": "morten"})
-# "SELECT * FROM tbl WHERE username = 'morten';"
-
-# >>> read("tbl", **{"user_type": 1, "user_group": "admin"})
-# "SELECT * FROM tbl WHERE user_type = '1' AND user_group = 'admin';"
+def rating():
+    """ Generates SQL for a SELECT statement matching the kwargs passed. """
+    pass
